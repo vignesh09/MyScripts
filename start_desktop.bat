9675050717e0f151be7ad7250ca3c0e7cc73ca98
Vignesh <vignez.sr@gmail.com>
2019-11-25 23:24:35 +0530

    Start up script
    All the programs that i wanted to start in three different virtual desktops - needs https://github.com/eksime/VDesk and Wins10

---
+++ start_desktop.bat
@@ -0,0 +1,12 @@
+vdesk on:1 run:"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" /new-window "http://aswathdamodaran.blogspot.com/" "https://www.livemint.com/" "https://economictimes.indiatimes.com/" "https://timesofindia.indiatimes.com/blogs/" "https://www.wired.com/" "https://hbr.org/" "https://www.vccircle.com/" "https://inc42.com/" "https://andrewchen.co/"
+sleep 5	
+vdesk on:1 run:"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" /new-window "https://mail.google.com/mail/u/0/#inbox" "https://mail.google.com/mail/u/1/#inbox" "https://mail.google.com/mail/u/3/#inbox" "https://kite.zerodha.com/" 
+sleep 5
+vdesk on:2 run:"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" /new-window "http://app.boombirds.com/" "http://sit-app.boombirds.com/" "https://www.boombirds.com/" "https://sso.godaddy.com/login?realm=pass&app=email" "https://www.dropbox.com/home/Softborne-Team/Timesheet?select=TimeSheet_RVignesh.xlsx" "https://trello.com/b/9Zn26MSr/customer-on-boarding"
+sleep 10
+vdesk on:2 run:"C:\Users\vignesh-pc\AppData\Local\slack\slack.exe"
+sleep 5
+vdesk on:3 run:"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" /new-window "https://www.linkedin.com/feed/" "https://www.naukri.com/myapply/historypage" "https://www.iimjobs.com/jobfeed"
+sleep 5
+vdesk on:2 run:"C:\Program Files\Microsoft Office\root\Office16\ONENOTE.exe"
+sleep 5
